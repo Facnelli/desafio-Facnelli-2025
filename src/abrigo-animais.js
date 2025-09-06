04c88753fbf1f01a3e820634928478f4921280e4
Facnelli <facnelli@gmail.com>
2025-09-06 19:14:51 -0300

    Feat: classe animal e array de animais

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,7 +1,23 @@
-class AbrigoAnimais {
-
-  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+class Animal {
+  constructor(nome, raca, ...brinquedos) {
+    this.nome = nome;
+    this.raca = raca;
+    this.brinquedos = brinquedos;
   }
+}
+
+const Animais = [
+  new Animal("Rex", "cão", "RATO", "BOLA"),
+  new Animal("Mimi", "gato", "BOLA", "LASER"),
+  new Animal("Fofo", "gato", "BOLA", "RATO", "LASER"),
+  new Animal("Zero", "gato", "RATO", "BOLA"),
+  new Animal("Bola", "cão", "CAIXA", "NOVELO"),
+  new Animal("Bebe", "cão", "LASER", "RATO", "BOLA"),
+  new Animal("Loco", "jabuti", "SKATE", "RATO"),
+];
+
+class AbrigoAnimais {
+  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {}
 }
 
 export { AbrigoAnimais as AbrigoAnimais };
