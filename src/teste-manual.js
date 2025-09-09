87a7ff4c4e4f92b71b8cf89b96f9643cba5e45bd
Facnelli <facnelli@gmail.com>
2025-09-09 14:21:14 -0300

    Fix: comentario e altyeraçã oarquivo de teste-manual

--- src/teste-manual.js
+++ src/teste-manual.js
@@ -1,4 +1,5 @@
 //Arquivo de teste manual
+//roda com "node teste-manual.js"
 
 import criarAnimais from "./data/Animais.js";
 import { AbrigoAnimais } from "./abrigo-animais.js";
@@ -7,10 +8,6 @@ const animais = criarAnimais();
 const abrigo = new AbrigoAnimais(animais);
 
 //cenário para o teste
-const resultado = abrigo.encontraPessoas(
-  "RATO,BOLA",
-  "RATO,NOVELO",
-  "Rex,Fofo"
-);
+const resultado = abrigo.encontraPessoas("RATO,BOLA", "RATO,NOVELO", "Rex,Fof");
 
 console.log("Resultado Final:", resultado);
